f200984b6539b0af2f99d4f151a82f1c1ef77f67
Stanislav Kazankov <s.v.kazankov@mail.ru>
2020-08-25 22:28:28 +0900

    Выполняет большую часть тестового задания

---
+++ gulpfile.js
@@ -0,0 +1,140 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemaps = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var uglify = require("gulp-uglify");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var server = require("browser-sync").create();
+
+gulp.task("nrmz", function () {
+  return gulp.src("source/css/normalize.css")
+    .pipe(csso())
+    .pipe(rename("normalize.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream())
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+      path.extname = ".js";
+    }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg,webp}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/{icon,logo}-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/css/**/*.{doc,docx}",
+    "source/fonts/**/*.{woff,woff2,otf,ttf,eot}",
+    "source/img/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/css/**/normalize.css", gulp.series("nrmz"));
+  gulp.watch("source/sass/**/*.scss", gulp.series("css", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
+  gulp.watch("source/img/**/*.{png,jpg,svg}", gulp.series("images", "refresh"));
+  gulp.watch("source/img/**/*.{png,jpg}", gulp.series("webp", "refresh"));
+  gulp.watch("source/img/{icon,logo}-*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "nrmz",
+  "css",
+  "js",
+  "images",
+  "webp",
+  "sprite",
+  "html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
---
+++ source/js/product-card.js
@@ -0,0 +1,107 @@
+'use strict';
+
+(function () {
+  var PINK = '#d91667';
+  var GREY = '#666666';
+  var SELECTED_HOVER_OVERTITLE_TEXT = 'Котэ не одобряет?';
+
+  var defaultPromoWords = document.querySelector('.product-card__promo-words').innerHTML;
+
+  var selectedPromoWordsMap = {
+    'product-card--foie-gras': 'Печень утки разварная с артишоками.',
+    'product-card--fish': 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+    'product-card--chicken': 'Филе из цыплят с трюфелями в бульоне.'
+  }
+
+  var disabledPromoWordsMap = {
+    'product-card--foie-gras': 'Печалька, с фуа-гра закончился.',
+    'product-card--fish': 'Печалька, с рыбой закончился.',
+    'product-card--chicken': 'Печалька, с курой закончился.'
+  }
+
+  var productList = document.querySelector('.products__list');
+
+  var defaultOvertitleText = document.querySelector('.product-card__overtitle').textContent;
+
+  var isNotPromoWords = function (elem) {
+    return !elem.classList.contains('product-card__promo-words') || elem.classList.contains('product-card__buy-word');
+  }
+
+  var getParentByClassName = function (elem, parentClassName) {
+    var currentElem = elem;
+    while (currentElem !== document && !currentElem.classList.contains(parentClassName)) {
+      currentElem = currentElem.parentNode;
+    }
+    if (currentElem === document || !currentElem.classList.contains(parentClassName)) {
+      return null;
+    }
+    return currentElem;
+  }
+
+  var setOvertitle = function (overtitle, overtitleText, overtitleColor) {
+    overtitle.textContent = overtitleText;
+    overtitle.style.color = overtitleColor;
+  }
+
+  productList.addEventListener('click', function (evt) {
+    if (isNotPromoWords(evt.target)) {
+      var productCard = getParentByClassName(evt.target, 'product-list__card');
+      if (productCard !== null) {
+        if (!productCard.classList.contains('product-card--disabled')) {
+          var overtitle = productCard.querySelector('.product-card__overtitle');
+          var promoWords = productCard.querySelector('.product-card__promo-words');
+          if (productCard.classList.contains('product-card--selected')) {
+            setOvertitle(overtitle, defaultOvertitleText, GREY);
+            promoWords.innerHTML = defaultPromoWords;
+          } else {
+            promoWords.textContent = selectedPromoWordsMap[productCard.classList[2]];
+          }
+          productCard.classList.toggle('product-card--selected');
+        }
+      }
+    }
+  });
+
+  var toggleProductCard = function (elem, overtitleText, overtitleColor) {
+    var productCard = getParentByClassName(elem, 'product-list__card');
+    if (productCard !== null) {
+      if (!productCard.classList.contains('product-card--disabled')) {
+        var overtitle = productCard.querySelector('.product-card__overtitle');
+        if (productCard.classList.contains('product-card--selected')) {
+          setOvertitle(overtitle, overtitleText, overtitleColor);
+        }
+        productCard.classList.toggle('product-card--default-hover');
+      }
+    }
+  }
+
+  var toggleProductCardByСondition = function (elem, overtitleText, overtitleColor) {
+    if (isNotPromoWords(elem)) {
+      toggleProductCard(elem, overtitleText, overtitleColor);
+    }
+  }
+
+  productList.addEventListener('mouseover', function (evt) {
+    toggleProductCardByСondition(evt.target, SELECTED_HOVER_OVERTITLE_TEXT, PINK);
+  });
+
+  productList.addEventListener('mouseout', function (evt) {
+    toggleProductCardByСondition(evt.target, defaultOvertitleText, GREY);
+  });
+
+  var disableProductCard = function (productCard) {
+    productCard.classList.add('product-card--disabled');
+    var promoWords = productCard.querySelector('.product-card__promo-words');
+    promoWords.textContent = disabledPromoWordsMap[productCard.classList[2]];
+  }
+
+  var makeAbledProductCard = function (productCard) {
+    productCard.classList.remove('product-card--disabled');
+    var promoWords = productCard.querySelector('.product-card__promo-words');
+    promoWords.innerHTML = defaultPromoWords;
+  }
+
+  var chickenProductCard = document.querySelector('.product-card--chicken');
+
+  disableProductCard(chickenProductCard);
+})();
